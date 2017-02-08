e71d35f78c538ffc74b8ece185e118d3f81982a7
Sibevin Wang <sibevin@gmail.com>
2017-02-09 00:17:56 +0800

    Implement metadata task
    1. Build POSTS const.

--- gulp-tasks/metadata.js
+++ gulp-tasks/metadata.js
@@ -4,6 +4,7 @@ const through = require('through2')
 const readline = require('readline')
 const cached = require('gulp-cached')
 const concat = require('gulp-concat')
+const uglify = require('gulp-uglify')
 
 var fetchAllMetadata = function(chunk, enc, cb) {
   var targetFolder = './src/posts/'
@@ -26,23 +27,40 @@ var fetchAllMetadata = function(chunk, enc, cb) {
 }
 
 var fetchMetadata = function(chunk, enc, cb) {
-  var rl = readline.createInterface({
-    input: String(chunk.contents)
-  }).on('line', (line) => {
+  var content = String(chunk.contents)
+  var lines = content.split("\n")
+  var data = {}
+  var reg = /^\.meta-data\ ([^ ]*)\ (.*)$/
+  for (var line of lines) {
     if (line.match(/^\.meta-data\ end/)) {
-      // stop reading
-      console.log('stop', line)
-    } else if (line.match(/^\.meta-data/)) {
-      console.log(line)
+      break
+    } else {
+      var result = line.match(reg)
+      if (result !== null) {
+        data[result[1]] = result[2]
+      }
     }
-  })
+  }
+  if (Object.keys(data).length === 0) {
+    chunk.contents = new Buffer('')
+  } else {
+    chunk.contents = new Buffer(JSON.stringify(data) + ',')
+  }
+  cb(null, chunk)
+}
+
+var buildConst = function(chunk, enc, cb) {
+  var content = 'var POSTS = [' + String(chunk.contents).slice(0, -2) + '];'
+  chunk.contents = new Buffer(content)
   cb(null, chunk)
 }
 
 module.exports = function() {
   return gulp.src('src/posts/*.slm')
-    .pipe(cached('metadata'))
+    //.pipe(cached('metadata'))
     .pipe(through.obj(fetchMetadata))
     .pipe(concat('metadata.js'))
+    .pipe(through.obj(buildConst))
+    .pipe(uglify())
     .pipe(gulp.dest('./src/js/consts/'))
 }
