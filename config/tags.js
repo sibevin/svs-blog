b6a765848e0b8d4e844fe64fc86e4aba7f004717
Sibevin Wang <sibevin@gmail.com>
2017-02-13 14:53:22 +0800

    Add tags config

---
+++ config/tags.js
@@ -0,0 +1,151 @@
+{
+  "ruby": {
+    "color": "#cc342d"
+  },
+  "rails": {
+    "color": "#cc342d"
+  },
+  "mysql": {
+    "name": "MySQL",
+    "color": "#015a84"
+  },
+  "git": {
+    "color": "#ef391a"
+  },
+  "ooad": {
+    "name": "OOAD",
+    "color": "#008040"
+  },
+  "rails_study-group": {
+    "name": "讀書會"
+  },
+  "vim": {
+    "color": "#007f00"
+  },
+  "js": {
+    "color": "#efc100"
+  },
+  "angularjs": {
+    "name": "AngularJS",
+    "color": "#b94a48"
+  },
+  "jasmine": {
+    "color": "#772a70"
+  },
+  "note": {
+    "name": "筆記",
+    "color": "#6699ff"
+  },
+  "tips": {
+    "name": "技巧",
+    "color": "#8f4700"
+  },
+  "cheat-sheet": {
+    "name": "小抄",
+    "color": "#008f6b"
+  },
+  "job": {
+    "name": "工作",
+    "color": "#c7c7c7"
+  },
+  "link": {
+    "name": "連結",
+    "color": "#0174df"
+  },
+  "funny": {
+    "name": "有趣",
+    "color": "#008f6b"
+  },
+  "cooking": {
+    "name": "料理",
+    "color": "#2b830a"
+  },
+  "work": {
+    "name": "作品",
+    "color": "#66ccff"
+  },
+  "devise": {
+    "color": "#0f4361"
+  },
+  "grape": {
+    "color": "#501f77"
+  },
+  "test": {
+    "name": "測試",
+    "color": "#0748b3"
+  },
+  "gh": {
+    "name": "Growth Hacking",
+    "color": "#993366"
+  },
+  "gh_dggh": {
+    "name": "DGGH"
+  },
+  "bootstrap": {
+    "color": "#563d7c"
+  },
+  "sass": {
+    "color": "#b3738a"
+  },
+  "atom": {
+    "color": "#40a977"
+  },
+  "mp": {
+    "name": "Metaprogramming",
+    "color": "#28784d"
+  },
+  "regex": {
+    "name": "Regular Expression",
+    "color": "#93bedf"
+  },
+  "nosql": {
+    "name": "NoSQL",
+    "color": "#375000"
+  },
+  "nsd": {
+    "name": "NoSQL Distilled",
+    "color": "#f5c531"
+  },
+  "english": {
+    "color": "#805315"
+  },
+  "alg": {
+    "name": "演算法",
+    "color": "#ff7400"
+  },
+  "book": {
+    "name": "書"
+  },
+  "book_poodr": {
+    "name": "POODR",
+    "color": "#cc342d"
+  },
+  "book_mr2": {
+    "name": "MR2",
+    "color": "#cc342d"
+  },
+  "book_tp": {
+    "name": "Test Prescription",
+    "color": "#cc342d"
+  },
+  "doc": {
+    "name": "文件",
+    "color": "#56ab00"
+  },
+  "blog": {
+    "name": "部落格",
+    "color": "#291657"
+  },
+  "dp": {
+    "name": "Design Pattern",
+    "color": "#5e4543"
+  },
+  "docker": {
+    "name": "Docker",
+    "color": "#2294ca"
+  },
+  "fp": {
+    "name": "Functional Programming",
+    "color": "#0e543f"
+  }
+}
